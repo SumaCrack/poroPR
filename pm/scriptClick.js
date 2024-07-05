0ca16f67dab342b900bdf54e4ed17a9f50baba69
SumaCrack <164176787+SumaCrack@users.noreply.github.com>
2024-07-05 07:31:33 -0300

    Pagina pp

---
+++ pm/scriptClick.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.heart').addEventListener('click', function() {
+        window.location.href = 'Principal.html'; // Cambia la URL por la que desees
+    });
+});
